44b1444102f638c90b9f9dafbf88ffdcf0fbd237
gugitgugit <rnwnsgur28@gmail.com>
2024-07-10 17:18:34 +0900

    useState ex

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import MyComponent from "./MyComponent";
+import Say from "./Say";
 
 const App = () => {
   return (
-    <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>
+    <>
+      <MyComponent name="React" favoriteNumber={1}>
+        리액트
+      </MyComponent>
+      <Say />
+    </>
   );
 };
 
