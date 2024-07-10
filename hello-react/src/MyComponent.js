1d434e4a35fba04639868dfbba7f3a1a731b9392
gugitgugit <rnwnsgur28@gmail.com>
2024-07-10 17:04:24 +0900

    create Counter

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const MyComponent = () => {
+const MyComponent = props => {
   return (
     <div>
-      나의 새롭고 멋진 컴포넌트
+      안녕하세요, 제 이름은 {props.name}입니다.
     </div>
   );
 };
